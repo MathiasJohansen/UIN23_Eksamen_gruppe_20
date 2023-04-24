64cb605ca3b1d508258c48d9ced65250551acf91
MathiasJohansen <111729300+MathiasJohansen@users.noreply.github.com>
2023-04-24 20:50:32 +0200

    La til i gameshop

--- spillbibliotek/src/components/Gameshop.js
+++ spillbibliotek/src/components/Gameshop.js
@@ -4,18 +4,36 @@ export default function Gameshop() {
     
     const [printgames, setGame] = useState([])
     
+    
     const getGames = async() => {
-        const response = await fetch ("https://api.rawg.io/api/platforms?key=d7e8ed9e06e04e6a8be1835df02b3a17")
+        const response = await fetch ("https://api.rawg.io/api/games?key=d7e8ed9e06e04e6a8be1835df02b3a17")
         const data = await response.json()
 
-        setGame(data)
+        const gameInfo = data?.results
+        setGame(gameInfo)
 
-        console.log(data, printgames)
+       
+
+        console.log(gameInfo)
         
     }
+
+    
+
     useEffect(() =>{
         getGames()
     },[])
-    return <h1>test gameshop</h1>
 
+    
+
+    return (
+        <>
+            <h1>Gameshop</h1>
+            <ul>
+                {printgames?.map((games, index) =>(
+                    <li><Gamecard key={index} title={games?.name} img={games?.background_image} genre={games?.genres}/></li>
+                ))}    
+            </ul>    
+        </>
+        )
 }

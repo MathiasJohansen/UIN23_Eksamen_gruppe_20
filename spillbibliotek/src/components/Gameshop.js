ddfb9df1a13ec06a05603543315423c733a24671
MathiasJohansen <111729300+MathiasJohansen@users.noreply.github.com>
2023-04-24 20:48:31 +0200

    Sitter fast med id og store arbeid

--- spillbibliotek/src/components/Gameshop.js
+++ spillbibliotek/src/components/Gameshop.js
@@ -1,21 +1,52 @@
 import { useEffect, useState } from "react";
+import Gamecard from "./Gamecard";
 
 export default function Gameshop() {
     
     const [printgames, setGame] = useState([])
+    const [gameId, setGameId] = useState([])
+    
     
     const getGames = async() => {
-        const response = await fetch ("https://api.rawg.io/api/platforms?key=d7e8ed9e06e04e6a8be1835df02b3a17")
+        const response = await fetch ("https://api.rawg.io/api/games?key=d7e8ed9e06e04e6a8be1835df02b3a17")
         const data = await response.json()
 
-        setGame(data)
+        const gameInfo = data?.results
+        setGame(gameInfo)
 
-        console.log(data, printgames)
+        const resultat = gameInfo?.map(info => info?.id)
+
+        const getGameStore = await Promise.all (resultat.map(id =>(
+            fetch (`https://api.rawg.io/api/stores/${id}`)
+        )))
+
+       console.log("getgamestore", getGameStore)
+
+       
+
+        console.log(gameInfo)
         
     }
+
+    
+
     useEffect(() =>{
         getGames()
     },[])
-    return <h1>test gameshop</h1>
+
+    {gameId.map((id, index) => {
+        <Gameshop id={id}/>
+    })}
+
+    return (
+        <>
+            <h1>Gameshop</h1>
+            <ul>
+                {printgames?.map((games, index) =>(
+                    <li><Gamecard key={index} title={games?.name} img={games?.background_image} genre={games?.genres}/></li>
+                ))}    
+            </ul>    
+        </>
+        )
 
 }

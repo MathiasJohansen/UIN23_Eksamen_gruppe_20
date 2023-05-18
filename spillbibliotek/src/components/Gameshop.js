b502989c0f29f091cd73babefd295796645e83d3
MathiasJohansen <111729300+MathiasJohansen@users.noreply.github.com>
2023-05-18 16:26:36 +0200

    weow

--- spillbibliotek/src/components/Gameshop.js
+++ spillbibliotek/src/components/Gameshop.js
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import Gamecard from "./Gamecard";
 import { Link } from "react-router-dom";
 
-
-
 export default function Gameshop(props) {
   const [printgames, setGame] = useState([]);
 
@@ -16,48 +14,48 @@ export default function Gameshop(props) {
     const gameInfo = data?.results;
     setGame(gameInfo);
 
-    console.log("hmmm",printgames);
+    console.log("hmmm", printgames);
   };
   useEffect(() => {
     getGames(props.ps ?? 10);
   }, []);
 
-  const [printstore, setStore] = useState([])
+  const [printstore, setStore] = useState([]);
 
   const getStore = async () => {
+    const gamesearch = printgames.map((items) => items?.id);
 
-    const gamesearch = printgames.map((items => items?.id))
+    const gameIds = await Promise.all(
+      gamesearch.map((id) =>
+        fetch(
+          `https://api.rawg.io/api/games/${id}/stores?&key=d7e8ed9e06e04e6a8be1835df02b3a17`
+        )
+      )
+    );
 
-    const gameIds = await Promise.all(gamesearch.map(id => (
-      fetch(`https://api.rawg.io/api/games/${id}/stores?&key=d7e8ed9e06e04e6a8be1835df02b3a17`)
-  )))
+    const storelinks = await Promise.all(
+      gameIds.map((gameresponse) => gameresponse.json())
+    );
+    const storelinksresult = storelinks.map((items) => items?.results);
 
-  const storelinks = await Promise.all(gameIds.map(gameresponse => (
-      gameresponse.json()
-  )))
-    const storelinksresult = storelinks.map((items => items?.results))
-    
+    setStore(storelinksresult);
+    console.log("data store", storelinks);
 
-    setStore(storelinksresult)
-console.log("data store", storelinks)
-    
-    console.log("test store", printstore)
-    
-  }
+    console.log("test store", printstore);
+  };
   useEffect(() => {
-    getStore() 
-  }, [])
+    getStore();
+  }, []);
 
   return (
     <section className={props.ps == 3 ? "dashboard-shop" : "gamepage"}>
-      <h2>GAMESHOP
-      <button className="redirect">
-          <Link to="/gameshop">
-            Visit shop
-          </Link>
+      <h2>
+        GAMESHOP
+        <button className="redirect">
+          <Link to="/gameshop">Visit shop</Link>
         </button>
       </h2>
-      
+
       <ul className={props.ps == 3 ? "dashboard-shop-list" : ""}>
         {printgames?.map((games, index) => (
           <li>
